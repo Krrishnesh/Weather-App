cfced43367bdf4974e27408314740ae2615f2c5a
Krrishnesh <krishneshkhalora98@gmail.com>
2021-09-16 23:27:08 +0530

    limit on get weather button press and border color added

--- app.js
+++ app.js
@@ -15,11 +15,25 @@ function getWeather(){
         i = i%4;
     }
     console.log(i)
-    if(i === 1) city = "London";
-    if(i === 2) city = "New York";
-    if(i === 3) city = "Los Angeles";
-    if(i === 4) city = "Las Vegas";
 
+    if(i === 1){
+      city = "London";
+      document.getElementById("London").style.border = "3px solid rgb(9, 114, 9)";
+    } 
+    if(i === 2) {
+      city = "New York";
+      document.getElementById("New York").style.border = "3px solid rgb(9, 114, 9)";
+    }
+    if(i === 3) {
+      city = "Los Angeles";
+      document.getElementById("Los Angeles").style.border = "3px solid rgb(9, 114, 9)";
+    }
+    if(i === 4) {
+      city = "Las Vegas";
+      document.getElementById("Las Vegas").style.border = "3px solid rgb(9, 114, 9)";
+    }
+
+    //Fetch Weather data by calling an api 
     fetch("https://python3-dot-parul-arena-2.uc.r.appspot.com/test?cityname="+city)
     .then(response => {
     if (!response.ok) {
@@ -32,15 +46,17 @@ function getWeather(){
      description.push(myData)
      currData = myData;
      var table = document.getElementById("myTable");
+     
+    //Inserting the row in the Table
      var row = table.insertRow(i+1);
+
      var cell1 = row.insertCell(0);
      var cell2 = row.insertCell(1);
      var cell3 = row.insertCell(2);
      var cell4 = row.insertCell(3);
      var cell5 = row.insertCell(4);
     
-   
-     var date_current = new Date()
+    var date_current = new Date()
      console.log(date_current)
      console.log((new Date() - new Date(myData.date_and_time))/(1000*3600*24))
 
@@ -54,6 +70,10 @@ function getWeather(){
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
   }); 
+
+  if(i >= 4){
+    document.getElementById("getWeatherBtn").disabled = true;
+  }
 
   if(i > 0){
     document.getElementById("noData").innerHTML = "";
